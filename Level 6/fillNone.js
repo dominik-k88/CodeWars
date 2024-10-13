57ad30f8bd481d4958b7dbf40945959ec817d1d8
dominik-k <dominik88k@seznam.cz>
2024-10-13 14:31:33 +0200

    add functions for last two parameters

--- Level 6/fillNone.js
+++ Level 6/fillNone.js
@@ -1,39 +1,82 @@
-const fillNone = (arr) => {
-    const numbers = []
-    arr.forEach((info,index) =>{
-        if(typeof info === "number")
-        numbers.push(index)
-    })
-    for(let i = 0; i < numbers.length; i++){
-        const numIndex = numbers[i]
-        if(typeof arr[numIndex - 1] === "undefined" && (numIndex - 1 ) >= 0){
-            arr[numIndex - 1] = arr[numIndex]
+const fillNone = (arr, method) => {
+    const param0 = arr => {
+        const numbers = []
+        arr.forEach((info,index) =>{
+            if(typeof info === "number")
+            numbers.push(index)
+        })
+        for(let i = 0; i < numbers.length; i++){
+            const numIndex = numbers[i]
+            if(typeof arr[numIndex - 1] === "undefined" && (numIndex - 1 ) >= 0){
+                arr[numIndex - 1] = arr[numIndex]
+            }
+            if(typeof arr[numIndex + 1] === "undefined" && (numIndex + 1 ) < arr.length){
+                arr[numIndex + 1] = arr[numIndex]
+            }
         }
-        if(typeof arr[numIndex + 1] === "undefined" && (numIndex + 1 ) < arr.length){
-            arr[numIndex + 1] = arr[numIndex]
-        }
-    }
-    for(let i = 0; i < arr.length; i++){
-        
-        if(arr[i] === undefined){
-         let indexOfNear = Infinity
-         let num = null
-         
-         for(let j = 0; j < arr.length; j++){
-             if( typeof arr[j] === "number"){
-                 let tempPosition = Math.abs( i - j)
-                 if(tempPosition < indexOfNear){
-                 indexOfNear = tempPosition
-                 num = arr[j]
-                     }else if( tempPosition === indexOfNear ){
-                 num = Math.min(num, arr[j])
+        for(let i = 0; i < arr.length; i++){
+            
+            if(arr[i] === undefined){
+             let indexOfNear = Infinity
+             let num = null
+             
+             for(let j = 0; j < arr.length; j++){
+                 if( typeof arr[j] === "number"){
+                     let tempPosition = Math.abs( i - j)
+                     if(tempPosition < indexOfNear){
+                     indexOfNear = tempPosition
+                     num = arr[j]
+                         }else if( tempPosition === indexOfNear ){
+                     num = Math.min(num, arr[j])
+                 }
+                 }
              }
-             }
-         }
-         arr[i] = num
+             arr[i] = num
+            }
         }
+        return arr
     }
-    return arr
+
+    const param_1 = arr => {
+        for(let i = 0; i < arr.length; i++) {
+            const el = arr[i]
+            if(el === undefined) {
+                for ( let j = i + 1; j < arr.length; j++) {
+                    const num = arr[j]
+                    if (typeof num === "number") {
+                        arr[i] = num;
+                        break;
+                    }
+                }
+            }
+        }
+        return arr
+    }
+    const param1 = arr => {
+        for(let i = arr.length - 1; i >= 0; i--) {
+            const el = arr[i]
+            if(el === undefined) {
+                for ( let j = i - 1; j >= 0; j--) {
+                    const num = arr[j]
+                    if (typeof num === "number") {
+                        arr[i] = num;
+                        break;
+                    }
+                }
+            }
+        }
+        return arr
+    }
+
+    switch ( method) {
+        case 0:
+            return param0(arr)
+        case -1:
+            return param_1(arr)
+        case 1:
+            return param1(arr)
+    }
+    
  }
  
- console.log(fillNone([undefined, 1, undefined, undefined, undefined, 2, undefined]));
+ console.log(fillNone([1,undefined],method = -1));
